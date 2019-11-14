ac5d9b69a182c315306c076634f899be55b7560a
Nory Lazo <nlazo@icloud.com>
2019-11-14 07:24:27 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -36,7 +36,13 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {
+          :name,
+          :age,
+          :attitude
+        }
+        ]
    }
   }
 
